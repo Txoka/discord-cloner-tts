943b51d31813069616d12048f4ce27a0f43037cf
Txoka <asiertxoka.2@gmail.com>
2026-01-29 14:47:29 +0100

    remove hardcoded superadmin id

--- app/config.py
+++ app/config.py
@@ -21,7 +21,7 @@ LOG_LEVEL = os.environ.get("QWEN_TTS_LOG_LEVEL", "INFO")
 
 # Admins
 DISCORD_ADMIN_ENABLED = os.environ.get("DISCORD_ADMIN_ENABLED", "1").lower() not in {"0", "false", "no", "off"}
-DISCORD_SUPERADMIN_ID = os.environ.get("DISCORD_SUPERADMIN_ID", "441597233150951425")
+DISCORD_SUPERADMIN_ID = os.environ.get("DISCORD_SUPERADMIN_ID", "")
 SUPERADMIN_IDS = [int(x) for x in (s.strip() for s in DISCORD_SUPERADMIN_ID.split(",")) if x]
 
 # Volume normalization (simple RMS normalization per utterance)
